b4ae1f06b904c8590999a8b099b5e37ca84f65eb
unknown <sivanhelpher@gmail.com>
2018-12-08 18:29:36 +0200

    First updload

---
+++ app/src/main/java/com/example/sivan/paah/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.sivan.paah;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timerTextView;
+    long startTime = 0;
+
+
+    //runs without a timer by reposting this handler at the end of the runnable
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            timerTextView.setText(String.format("%d:%02d:%02d", minutes, seconds, millis));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerTextView = findViewById(R.id.timerTextView);
+        Button b = findViewById(R.id.timerButton);
+        b.setText("start");
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (b.getText().equals("stop")) {
+                    timerHandler.removeCallbacks(timerRunnable);
+                    b.setText("start");
+                } else {
+                    startTime = System.currentTimeMillis();
+                    timerHandler.postDelayed(timerRunnable, 0);
+                    b.setText("stop");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+        Button b = findViewById(R.id.timerButton);
+        b.setText("start");
+    }
+}
