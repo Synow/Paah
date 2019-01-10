6346ea1b70429565d136c3614ef494948400cd59
Sivan <sivanhelpher@gmail.com>
2019-01-11 01:12:04 +0200

    Added sensitivity control

--- app/src/main/java/com/example/sivan/paah/MainActivity.java
+++ app/src/main/java/com/example/sivan/paah/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.sivan.paah;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity  {
 
-    Stopwatch stopwatch = new Stopwatch();
+    Stopwatch stopwatch;
     TextView tvTextView;
+    TextView tvVolumeView;
+    ImageView ivCircle;
     Button btnStart;
     Timer timer;
+    Soundmeter soundmeter;
+    SeekBar sbSeekBar;
+    boolean lock = false;
 
     private void setTimer(Timer t){
         t.scheduleAtFixedRate(new TimerTask(){
@@ -39,11 +52,59 @@ public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //checking the permission status
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                //request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        1);
+            } else {} // ????
+        }
+
+
         tvTextView = findViewById(R.id.timerTextView);
         btnStart = findViewById(R.id.timerButton);
+        tvVolumeView = findViewById(R.id.volumeTextView);
+        ivCircle = findViewById(R.id.imageView);
+        sbSeekBar = findViewById(R.id.SensitivityBar);
+
+        Timer tmrVolChecker = new Timer();
+        tmrVolChecker.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        double volumeLevel = soundmeter.getAmplitude();
+                        tvVolumeView.setText("" + volumeLevel);
+                        if(lock) {
+                            lock = false;
+                            return;
+                        }
+                        if (volumeLevel > 400 * (100 - sbSeekBar.getProgress())) {
+                            ivCircle.setVisibility(View.VISIBLE);
+                            btnStart.performClick();
+                            lock = true;
+                        } else {
+                            ivCircle.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        },0,200);
+
+
+
+
+        stopwatch = new Stopwatch();
+        soundmeter = new Soundmeter();
+        soundmeter.start();
 
         btnStart.setOnClickListener(new OnClickListener() { // Previusly - ' setOnClickListener(this);'
             @Override
@@ -63,5 +124,19 @@ public class MainActivity extends AppCompatActivity  {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        stopwatch.stop();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        stopwatch.stop();
     }
 }
